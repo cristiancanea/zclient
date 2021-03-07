89f69e2456df6e33c5a1491a4ed478f23f096bae
CCA <cristian.canea@gmail.com>
2021-03-07 17:57:01 +0200

    run client-generator react for customer api resource

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducer as form } from 'redux-form';
+import { Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import {
+    ConnectedRouter,
+    connectRouter,
+    routerMiddleware
+} from 'connected-react-router';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//import * as serviceWorker from './serviceWorker';
+
+import App from './App';
+
+// Import your reducers and routes here
+import {Create, List, Show, Update} from "./components/customer";
+
+const history = createBrowserHistory();
+const store = createStore(
+    combineReducers({
+        router: connectRouter(history),
+        form,
+        /* Add your reducers here */
+    }),
+    applyMiddleware(routerMiddleware(history), thunk)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route path="/" component={App} exact key="home" />
+
+                <Route path="/customers/create" component={Create} exact key="create" />
+                <Route path="/customers/edit/:id" component={Update} exact key="update" />
+                <Route path="/customers/show/:id" component={Show} exact key="show" />
+                <Route path="/customers/" component={List} exact strict key="list" />
+                <Route path="/customers/:page" component={List} exact strict key="page" />
+
+                <Route render={() => <h1>Not Found</h1>} />
+            </Switch>
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
